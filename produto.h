b55d8edc44a8ab362c33cbf66673e80c1b037f87
athlas <elderluiz72@gmail.com>
2026-08-19 13:34:14 -0300

    fix: fixing merge conflicts with produto.h

--- produto.h
+++ produto.h
@@ -7,4 +7,14 @@ typedef struct {
     int quantidade;
 } produto_t;
 
-void listar_produto (produto_t *item, int tam, int base);
+produto_t *cadastra_produto_novo(produto_t produto_novo);
+
+int cadastra_produto(produto_t **lista_produto, int size_lista, produto_t produto_novo);
+
+produto_t busca_produto_id(produto_t *lista_item, int search_id, int size_lista);
+
+void remove_produto(produto_t v[], int id, int tam);
+
+float calc_estoque(produto_t vet[], int tam, int i);
+
+void encerra_programa(produto_t **listas, int size_listas);
