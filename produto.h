2a02a8d4e0c6b3c6c00aecc9989856c13bf50f10
santtos0x1 <ruanrodrigues393@gmail.com>
2026-08-18 22:28:06 -0300

    feat: adding 'cadastra produto' functionality

--- produto.h
+++ produto.h
@@ -6,3 +6,7 @@ typedef struct {
     float preco;
     int quantidade;
 } produto_t;
+
+produto_t *cadastra_produto_novo(produto_t produto_novo);
+
+int cadastra_produto(produto_t *lista_produto, int size_lista, produto_t produto_novo);
